900059e0d7631f869820501b7e193d57640b5612
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 15:15:58 -0400

    * added statements for user to input information * added comments for what needs to be completed next

--- src/test/java/com/pluralsight/TheaterReservations.java
+++ src/test/java/com/pluralsight/TheaterReservations.java
@@ -7,9 +7,14 @@ public class TheaterReservations {
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Please enter your full name: ");
+        String fullName = keyboard.nextLine().trim();
 
         System.out.println("Please enter the date of the movie in the following format (MM/dd/yyyy): ");
 
         System.out.println("Please enter the amount of tickets you need: ");
-    }
+    } // end of main()
+
+    //TO-DO: add statements for user input on date, ticket number, display message to user
+    // converting date to the standard version so program can print it out correctly
+    // two if statements to differentiate 1 ticket vs 2+ tickets in display message
 }
