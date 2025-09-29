ba1d5ab5707c12541a7cddb78ae74284d52c03a6
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 15:09:37 -0400

    * created sout statements asking the user for their name, the date they want to see the movie, and the number of tickets they need

--- src/test/java/com/pluralsight/TheaterReservations.java
+++ src/test/java/com/pluralsight/TheaterReservations.java
@@ -1,7 +1,15 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class TheaterReservations {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter your full name: ");
+
+        System.out.println("Please enter the date of the movie in the following format (MM/dd/yyyy): ");
+
+        System.out.println("Please enter the amount of tickets you need: ");
     }
 }
