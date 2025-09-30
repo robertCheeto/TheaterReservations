9509970710d87803b2ca32adadc72db73e83eb35
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 10:11:05 -0400

    * changed the fullName variable to an array and edited variable declaration so first and last names are split into two separate indexes * working on adding LocalDate and formatting the user date so I can use that data in the program display message

--- src/test/java/com/pluralsight/TheaterReservations.java
+++ src/test/java/com/pluralsight/TheaterReservations.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TheaterReservations {
@@ -7,12 +9,17 @@ public class TheaterReservations {
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Please enter your full name: ");
-        String fullName = keyboard.nextLine().trim();
+        String fullName[] = keyboard.nextLine().trim().split(" ");
 
-        System.out.println("Please enter the date of the movie in the following format (MM/dd/yyyy): ");
+        System.out.print("Please enter the date of the movie in the following format (MM/dd/yyyy): ");
         String movieDate = keyboard.nextLine().trim();
+        LocalDate format = movieDate.ofPattern("MM/dd/yyyy");
+        //DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
-        System.out.println("Please enter the amount of tickets you need: ");
+
+
+
+        System.out.print("Please enter the amount of tickets you need: ");
         int ticketCount = keyboard.nextInt();
 
         if (ticketCount == 1) {
@@ -28,9 +35,10 @@ public class TheaterReservations {
         }
     } // end of main()
 
+    // display message format# ticket(s) reserved for (date) under (LastName, FirstName)
 
     public static void singleTicket() {
-        System.out.println("Single ticket");
+        //System.out.printf("%d ticket reserved for %s under %s, %s.", ticketCount, date, lastName, firstName);
 
     } // end of singleTicket
 
