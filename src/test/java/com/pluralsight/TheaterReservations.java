c3a410d64dd76fca6323100da3b02bf41e757139
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 10:30:58 -0400

    * ensured the DateTimeFormatter and LocalDate were packages imported * correctly formatted the date by going back and looking at the workbook

--- src/test/java/com/pluralsight/TheaterReservations.java
+++ src/test/java/com/pluralsight/TheaterReservations.java
@@ -12,29 +12,24 @@ public class TheaterReservations {
         String fullName[] = keyboard.nextLine().trim().split(" ");
 
         System.out.print("Please enter the date of the movie in the following format (MM/dd/yyyy): ");
-        String movieDate = keyboard.nextLine();
-        System.out.println(movieDate);
-        //LocalDate today = LocalDate.parse(movieDate);
-        //System.out.println(today.format(DateTimeFormatter.ISO_DATE));
-
+        String movieDateInput = keyboard.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate movieDateOutput = LocalDate.parse(movieDateInput, formatter);
 
         System.out.print("Please enter the amount of tickets you need: ");
         int ticketCount = keyboard.nextInt();
 
+
         if (ticketCount == 1) {
-            System.out.printf("%d ticket reserved for %s under %s, %s.", ticketCount, movieDate, fullName[1], fullName[0]);
+            System.out.printf("%d ticket reserved for %s under %s, %s.", ticketCount, movieDateOutput, fullName[1], fullName[0]);
         } // end of if
 
         else if (ticketCount > 1) {
-            System.out.printf("%d tickets reserved for %s under %s, %s.", ticketCount, movieDate, fullName[1], fullName[0]);
+            System.out.printf("%d tickets reserved for %s under %s, %s.", ticketCount, movieDateOutput, fullName[1], fullName[0]);
         } // end of else-if
 
         else {
             System.out.println("No tickets were bought!");
         }
     } // end of main()
-
-    //TO-DO: add statements for user input on date, ticket number, display message to user
-    // converting date to the standard version so program can print it out correctly
-    // two if statements to differentiate 1 ticket vs 2+ tickets in display message
 }
